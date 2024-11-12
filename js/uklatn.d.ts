d9f7c2c6ad196d5080305f6393465a42822067ad
Pavlo Ivashkov <pavels.code@gmail.com>
2024-11-12 19:22:58 +0200

    publish to jsr

---
+++ js/uklatn.d.ts
@@ -0,0 +1,24 @@
+/**
+* Transliterates a string of Ukrainian Cyrillic to Latin script.
+*
+* @param {string} text - the text to transliterate
+* @param {string} table - transliteration system, one of:
+*  - "DSTU_9112_A": DSTU 9112:2021 System A
+*  - "DSTU_9112_B": DSTU 9112:2021 System B
+*  - "KMU_55": KMU 55:2010
+*/
+export function encode(text: string, table: string): string;
+/**
+* Re-transliterates a string of Ukrainian Latin to Cyrillic script.
+*
+* @param {string} text - the text to transliterate
+* @param {string} table - transliteration system, one of:
+*  - "DSTU_9112_A": DSTU 9112:2021 System A
+*  - "DSTU_9112_B": DSTU 9112:2021 System B
+*/
+export function decode(text: string, table: string): string;
+declare namespace _default {
+    export { encode };
+    export { decode };
+}
+export default _default;
